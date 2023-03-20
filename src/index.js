57cdfa0b4e55e2e399e5b4e513b77478bfbc1e38
kheersagar <santparja@gmail.com>
2023-03-20 11:22:45 +0530

    added toast to signup and login

--- src/index.js
+++ src/index.js
@@ -1,38 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, } from "react-router-dom";
-import { RouterProvider } from 'react-router';
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router";
 import Login from "./screens/login";
 import SignUp from "./screens/signup";
 import Dashboard from "./screens/dashboard";
 import Test from "./screens/test";
-
+import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />   
-  }, 
+    element: <Login />,
+  },
   {
     path: "/signup",
-    element: <SignUp />   
-  }, 
+    element: <SignUp />,
+  },
   {
     path: "/dashboard",
-    element: <Dashboard />   
-  }, 
+    element: <Dashboard />,
+  },
   {
     path: "/test",
-    element: <Test />   
-  }, 
-])
-
+    element: <Test />,
+  },
+]);
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+<Toaster
+  position="top-center"
+  reverseOrder={false}
+  gutter={8}
+  containerClassName=""
+  containerStyle={{}}
+  toastOptions={{
+    // Define default options
+    className: '',
+    duration: 5000,
+    style: {
+      background: '#363636',
+      color: '#fff',
+    },
+
+    // Default options for specific types
+    success: {
+      duration: 3000,
+      theme: {
+        primary: 'green',
+        secondary: 'black',
+      },
+    },
+  }}
+/>
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
